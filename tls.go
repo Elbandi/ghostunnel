98db7f349e6833154a540d2ce4a46feaf132cfb5
Elbandi <elso.andras@gmail.com>
2019-01-17 18:03:59 +0100

    Add more cipher suites

--- tls.go
+++ tls.go
@@ -38,6 +38,23 @@ var cipherSuites = map[string][]uint16{
 		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 	},
+	"CBC": {
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+	},
+	"RSA": {
+		tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+	},
 }
 
 // Build reloadable certificate
@@ -124,6 +141,8 @@ func buildConfig(enabledCipherSuites string, caBundlePath string) (*tls.Config, 
 			// P-256/X25519 have an ASM implementation, others do not (at least on x86-64).
 			tls.X25519,
 			tls.CurveP256,
+			tls.CurveP384,
+			tls.CurveP521,
 		},
 	}, nil
 }
